69322fa08c2cd3bbf6ddbeff2e5d962ced277544
solano-h <solano33@163.com>
2024-08-27 23:47:01 +0800

    feat: 添加jmh模块-临时

---
+++ jmh-demo/src/main/java/com/solano/order/OrderServerApplication.java
@@ -0,0 +1,22 @@
+package com.solano.order;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author solano
+ * @date 2024/7/5 16:36
+ */
+@Slf4j
+@ComponentScan(basePackages = "com.solano")
+@SpringBootApplication
+public class OrderServerApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(OrderServerApplication.class);
+        log.debug("applicationContext: {}", applicationContext);
+    }
+}
