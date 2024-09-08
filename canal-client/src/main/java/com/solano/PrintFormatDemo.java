c975d0aaf7a48c19ddd9ba5e216f6922c8f5af94
solano-h <solano33@163.com>
2024-09-08 23:59:38 +0800

    feat: canal-demo

---
+++ canal-client/src/main/java/com/solano/PrintFormatDemo.java
@@ -0,0 +1,12 @@
+package com.solano;
+
+/**
+ * @author github.com/solano33
+ * @date 2024/9/8 23:47
+ */
+public class PrintFormatDemo {
+
+    public static void main(String[] args) {
+        System.out.printf("%-4s: %-4s", "id", "666");
+    }
+}
---
+++ canal-client/src/main/java/com/solano/SimpleCanalClientExample.java
@@ -0,0 +1,112 @@
+package com.solano;
+
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+import com.alibaba.otter.canal.client.CanalConnectors;
+import com.alibaba.otter.canal.client.CanalConnector;
+import com.alibaba.otter.canal.common.utils.AddressUtils;
+import com.alibaba.otter.canal.protocol.Message;
+import com.alibaba.otter.canal.protocol.CanalEntry.Column;
+import com.alibaba.otter.canal.protocol.CanalEntry.Entry;
+import com.alibaba.otter.canal.protocol.CanalEntry.EntryType;
+import com.alibaba.otter.canal.protocol.CanalEntry.EventType;
+import com.alibaba.otter.canal.protocol.CanalEntry.RowChange;
+import com.alibaba.otter.canal.protocol.CanalEntry.RowData;
+
+/**
+ * @author github.com/solano33
+ * @date 2024/9/8 23:47
+ */
+public class SimpleCanalClientExample {
+
+
+    public static void main(String args[]) {
+        // 创建链接
+        CanalConnector connector = CanalConnectors.newSingleConnector(
+                new InetSocketAddress(AddressUtils.getHostIp(), 11111), "example", "", "");
+        int batchSize = 1000;
+        int emptyCount = 0;
+        try {
+            connector.connect();
+            connector.subscribe(".*\\..*");
+            connector.rollback();
+            int totalEmptyCount = 120;
+            while (emptyCount < totalEmptyCount) {
+                Message message = connector.getWithoutAck(batchSize); // 获取指定数量的数据
+                long batchId = message.getId();
+                int size = message.getEntries().size();
+                if (batchId == -1 || size == 0) {
+                    emptyCount++;
+                    System.out.println("empty count : " + emptyCount);
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException ignored) {
+                    }
+                } else {
+                    emptyCount = 0;
+                    // System.out.printf("message[batchId=%s,size=%s] \n", batchId, size);
+                    printEntry(message.getEntries());
+                }
+
+                connector.ack(batchId); // 提交确认
+                // connector.rollback(batchId); // 处理失败, 回滚数据
+            }
+
+            System.out.println("empty too many times, exit");
+        } finally {
+            connector.disconnect();
+        }
+    }
+
+    private static void printEntry(List<Entry> entrys) {
+        for (Entry entry : entrys) {
+            if (entry.getEntryType() == EntryType.TRANSACTIONBEGIN || entry.getEntryType() == EntryType.TRANSACTIONEND) {
+                continue;
+            }
+
+            RowChange rowChage = null;
+            try {
+                rowChage = RowChange.parseFrom(entry.getStoreValue());
+            } catch (Exception e) {
+                throw new RuntimeException("ERROR ## parser of eromanga-event has an error , data:" + entry, e);
+            }
+
+            EventType eventType = rowChage.getEventType();
+            System.out.println(String.format("receive===>: binlog[%s:%s] , name[%s,%s] , eventType : %s",
+                    entry.getHeader().getLogfileName(), entry.getHeader().getLogfileOffset(),
+                    entry.getHeader().getSchemaName(), entry.getHeader().getTableName(),
+                    eventType));
+
+            for (RowData rowData : rowChage.getRowDatasList()) {
+                if (eventType == EventType.DELETE) {
+                    printColumn(rowData.getBeforeColumnsList());
+                } else if (eventType == EventType.INSERT) {
+                    printColumn(rowData.getAfterColumnsList());
+                } else {
+                    System.out.print("\t=====> before: ");
+                    printColumn(rowData.getBeforeColumnsList());
+                    System.out.print("\t=====> after : ");
+                    printColumn(rowData.getAfterColumnsList());
+                }
+            }
+        }
+    }
+
+    private static void printColumn1(List<Column> columns) {
+        for (Column column : columns) {
+            System.out.printf("%5s: %-10s, updated: %b\n", column.getName(), column.getValue(), column.getUpdated());
+        }
+    }
+
+    private static void printColumn(List<Column> columns) {
+        Map<String, String> value = new HashMap<>();
+        for (Column column : columns) {
+            value.put(column.getName(), column.getValue());
+        }
+        System.out.println(value);
+    }
+}
